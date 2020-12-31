9b3e888830590f58b26a879164dc59e43492e3c0
Omri <omri206attiya@gmail.com>
2020-12-31 17:05:04 +0200

    change link to be available at all devices

--- src/services/instagramRequests.js
+++ src/services/instagramRequests.js
@@ -6,7 +6,7 @@ export const instagramRequests = (function () {
 
     async function getFollowersAndFollowing() {
         try {
-            return (await fetch(`http://localhost:3001/api/instagram`)).json();
+            return (await fetch(`http://192.168.1.13:3001/api/instagram`)).json();
         } catch (err) {
             return {followers: [], following: []}
         }
