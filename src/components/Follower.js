747ec33f34fb2cd785e6e1e8c88db7c8479b4333
Omri <omri206attiya@gmail.com>
2020-12-31 15:27:20 +0200

    make user clickable link to profile

--- src/components/Follower.js
+++ src/components/Follower.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import './follower.css';
 
+const INSTAGRAM_ADDRESS = 'https://www.instagram.com';
+
 export function Follower({username, fullName, profilePicUrl, type}) {
+
+    function generateProfileLink() {
+        return `${INSTAGRAM_ADDRESS}/${username}`;
+    }
 
     return (
         <div className={'inline'}>
             <div className={['user-wrapper', type].join(' ')}>
             <div className={'details-section'}>
-                <div className={'username'}>{username}</div>
+                <a href={generateProfileLink()} target={'_blank'} className={'username'}>{username}</a>
                 <div className={'full-name'}>{fullName || <>&nbsp;</>}</div>
             </div>
             <div>
-                <img className={'profile-pic ' + type} src={profilePicUrl}/>
+                <img className={'profile-pic ' + type} src={profilePicUrl} alt={'profile pic'}/>
             </div>
             </div>
         </div>
