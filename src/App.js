e72e4138c699c1e93d254b4e308acc9a590989a8
Omri <omri206attiya@gmail.com>
2020-12-31 09:27:03 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import {instagramRequests} from "./instagramRequests";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let [followers, setFollowers] = useState([]);
+    let [following, setFollowing] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            let instagram = await instagramRequests.getFollowersAndFollowing();
+            setFollowers(instagram?.followers || []);
+            setFollowing(instagram?.following || []);
+        })();
+    }, []);
+
+    return (
+        <div>
+          {
+            followers.map(follower => (
+                <div key={follower.username}>
+                  {follower.username}
+                </div>
+            ))
+          }
+        </div>
+    );
 }
 
 export default App;
