980caf3c796cb3b6b71882f7a279544e8ab9d298
Omri <omri206attiya@gmail.com>
2021-01-15 21:05:48 +0200

    limit username texts

--- src/components/follower/Follower.js
+++ src/components/follower/Follower.js
@@ -13,9 +13,14 @@ export function Follower({username, fullName, profilePicUrl, type, isNew = false
         <div className={'inline'}>
             <div className={['user-wrapper', type].join(' ')}>
                 <div className={'details-section'}>
-                    <a href={generateProfileLink()} target={'_blank'} rel="noopener noreferrer"
-                       className={'username'}>{username}</a>
-                    <div className={'full-name'}>{fullName || <>&nbsp;</>}</div>
+                    <a href={generateProfileLink()}
+                       target={'_blank'}
+                       rel="noopener noreferrer"
+                       className={'username'}
+                       tooltip={username}>
+                        {username}
+                    </a>
+                    <div className={'full-name'} tooltip={fullName}>{fullName || <>&nbsp;</>}</div>
                 </div>
                 <div>
                     <img className={'profile-pic ' + type} src={profilePicUrl} alt={'profile pic'}/>
