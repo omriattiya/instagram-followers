8ce05d4e1e61a4465bcd901760ed92ff878fd917
omri206attiya@gmail.com <dontthinkicandoit>
2021-10-09 15:31:45 +0300

    toggle sections

--- src/components/section/Section.js
+++ src/components/section/Section.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Follower} from "../follower/Follower";
 import './section.css';
 
 export function Section({title, followersList, type}) {
 
+    const [showContent, setShowContent] = useState(true);
+
+    const toggleShowContent = () => setShowContent(!showContent);
+
     return (
         <div>
-            <div className={'title'}>{title}</div>
+            <div
+                className="title"
+                onClick={toggleShowContent}
+            >
+                {`${title} (${followersList.length})`}
+            </div>
             <hr/>
             {
+                showContent &&
                 followersList.map(user => (
                     <Follower
                         username={user.username}
