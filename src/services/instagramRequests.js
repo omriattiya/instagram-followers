9877d9a2db385eeb24cd32ad8c005731e16e8cfc
omri206attiya@gmail.com <dontthinkicandoit>
2021-10-09 15:31:00 +0300

    change default port

--- src/services/instagramRequests.js
+++ src/services/instagramRequests.js
@@ -6,7 +6,7 @@ export const instagramRequests = (function () {
 
     async function getFollowersAndFollowing() {
         try {
-            return (await fetch(`http://192.168.1.13:3001/api/instagram`)).json();
+            return (await fetch(`http://192.168.1.25:8080/api/instagram`)).json();
         } catch (err) {
             return {followers: [], following: []}
         }
