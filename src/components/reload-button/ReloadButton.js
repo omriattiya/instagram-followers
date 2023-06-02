838fec90218a8c696f1ff8ab546fb6b7b994f74c
omri_attiya <omri206attiya@gmail.com>
2023-06-02 21:59:46 +0300

    loader on the reload button

--- src/components/reload-button/ReloadButton.js
+++ src/components/reload-button/ReloadButton.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import Loader from "react-loader-spinner";
 import './reloadButton.css';
 
 export function ReloadButton({reload, isLoading}) {
     return (
-        <button disabled={isLoading} id="reload" onClick={reload}>Reload</button>
-    );
+        <button disabled={isLoading} id="reload" onClick={reload}>
+            {isLoading ?
+                (
+                    <Loader width={20} height={20} type="Oval" color="#00BFFF"/>
+                ) : (
+                    <>Reload</>
+                )
+            }
+        </button>);
 }
