2d443a5cf18b5a2496a534378cf1a24d257f5172
omri_attiya <omri206attiya@gmail.com>
2023-09-23 18:34:21 +0300

    make style to fit phone screen as well

--- src/styled.js
+++ src/styled.js
@@ -12,43 +12,40 @@ export const ReloadContainer = styled.div`
 export const ScreenWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100vh;
-  width: 100vw;
 `;
 
 export const Title = styled.div`
-  height: 20vh;
+  height: 10vh;
+  padding: 30px 30px 0 30px;
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 100%;
   background: linear-gradient(0deg, #222D2F 0%, #20394d 25%, #1ea7fd 100%);
   text-transform: uppercase;
-  font-size: 5em;
+  font-size: 3em;
 `
 
 export const Content = styled.div`
   display: flex;
-  flex-direction: row;
-  height: 80vh;
+  flex-direction: column;
   padding: 30px 0;
-  width: 100vw;
+  flex-wrap: wrap;
 `;
 
 export const NavBar = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   justify-content: center;
   align-items: center;
-  align-self: center;
+  flex-wrap: wrap;
 `;
 
 export const InstagramUserTypeButton = styled.button`
   background: #1ea7fd;
   color: white;
-  height: 60px;
-  width: 15vw;
-  min-width: 250px;
+  height: 40px;
+  max-width: 10vw;
+  min-width: 200px;
   margin: 1em 2em;
   outline: none;
   border-radius: 15px;
@@ -57,7 +54,7 @@ export const InstagramUserTypeButton = styled.button`
   transition: 0.2s;
   font-family: Tahoma, serif;
   text-transform: uppercase;
-  font-size: 1em;
+  font-size: 0.8em;
 
   &:hover {
     background: #3db5ff;
